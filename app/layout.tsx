ac238417b743652a7b8fc0d2cd3c17bd767d78ce
Jacky Liang <liangj97@gmail.com>
2023-11-18 11:33:23 +1100

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react'
 import type { Metadata } from 'next'
 import { Josefin_Sans } from 'next/font/google'
 import './globals.css'
@@ -20,7 +21,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body className={josefinSans.className}>{children}</body>
+      <body className={josefinSans.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   )
 }
